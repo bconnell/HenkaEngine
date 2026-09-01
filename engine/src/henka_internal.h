db1328dd547f46f4e077bab67c39f09f766ea4b2
bconnell <brandon@brandonconnell.com>
2026-08-31 20:01:40 -0500

    refactor: centralize scene storage layout

--- engine/src/henka_internal.h
+++ engine/src/henka_internal.h
@@ -20,6 +20,8 @@
 #include <henka/shader.h>
 #include <henka/texture.h>
 
+#include "scene/scene_internal.h"
+
 #if defined(HENKA_WITH_KTX2_TRANSCODER)
 typedef enum henka_ktx2_gpu_format
 {
@@ -178,33 +180,6 @@ typedef struct henka_tool_window_slot
     henka_window_id id;
     struct henka_ui_context* ui_context;
 } henka_tool_window_slot;
-
-typedef struct henka_scene_entity_record
-{
-    bool active;
-    uint64_t generation;
-    bool visible;
-    uint32_t flags;
-    henka_entity selection_owner;
-    henka_entity parent;
-    char* name;
-    char* tag;
-    henka_transform local_transform;
-    henka_transform transform;
-    henka_transform previous_transform;
-    bool previous_transform_valid;
-    henka_mesh* mesh;
-    henka_scene_lod_desc lod;
-    henka_material material;
-    const henka_material_asset* material_asset;
-    uint64_t material_asset_revision;
-    bool material_asset_overridden;
-    char* material_name;
-    bool has_local_bounds;
-    henka_bounds local_bounds;
-    henka_interaction_desc interaction;
-    char* interaction_prompt;
-} henka_scene_entity_record;
 
 /* Renderer and editor internals iterate the backing slot array directly.
  * This is deliberately separate from the public active-ordinal accessor. */
@@ -379,27 +354,6 @@ struct henka_asset_manager
     henka_texture_residency_io_completion texture_residency_io_completion;
     uint64_t texture_residency_io_sequence;
     uint64_t texture_residency_io_cancel_before;
-};
-
-struct henka_scene
-{
-    henka_scene_entity_record* entities;
-    size_t entity_capacity;
-    size_t entity_count;
-    henka_camera camera;
-    bool has_camera;
-    henka_vec3 light_direction;
-    henka_vec3 light_color;
-    float light_intensity;
-    henka_vec3 ambient_color;
-    henka_scene_environment_desc environment;
-    henka_scene_reflection_probe_desc reflection_probes[HENKA_SCENE_MAX_REFLECTION_PROBES];
-    bool reflection_probe_active[HENKA_SCENE_MAX_REFLECTION_PROBES];
-    uint64_t render_revision;
-    uint64_t content_revision;
-    henka_scene_light_desc local_lights[HENKA_SCENE_MAX_LOCAL_LIGHTS];
-    bool local_light_active[HENKA_SCENE_MAX_LOCAL_LIGHTS];
-    henka_scene_fog_desc fog;
 };
 
 /* Internal bridge used by the asset manager to publish a refreshed
--- engine/src/runtime_internal.h
+++ engine/src/runtime_internal.h
@@ -7,52 +7,6 @@
 
 #include <henka/runtime.h>
 
-typedef struct henka_scene_entity_record
-{
-    bool active;
-    uint64_t generation;
-    bool visible;
-    uint32_t flags;
-    henka_entity selection_owner;
-    henka_entity parent;
-    char* name;
-    char* tag;
-    henka_transform local_transform;
-    henka_transform transform;
-    henka_transform previous_transform;
-    bool previous_transform_valid;
-    henka_mesh* mesh;
-    henka_scene_lod_desc lod;
-    henka_material material;
-    const henka_material_asset* material_asset;
-    uint64_t material_asset_revision;
-    bool material_asset_overridden;
-    char* material_name;
-    bool has_local_bounds;
-    henka_bounds local_bounds;
-    henka_interaction_desc interaction;
-    char* interaction_prompt;
-} henka_scene_entity_record;
-
-struct henka_scene
-{
-    henka_scene_entity_record* entities;
-    size_t entity_capacity;
-    size_t entity_count;
-    henka_camera camera;
-    bool has_camera;
-    henka_vec3 light_direction;
-    henka_vec3 light_color;
-    float light_intensity;
-    henka_vec3 ambient_color;
-    henka_scene_environment_desc environment;
-    henka_scene_reflection_probe_desc reflection_probes[HENKA_SCENE_MAX_REFLECTION_PROBES];
-    bool reflection_probe_active[HENKA_SCENE_MAX_REFLECTION_PROBES];
-    uint64_t render_revision;
-    uint64_t content_revision;
-    henka_scene_light_desc local_lights[HENKA_SCENE_MAX_LOCAL_LIGHTS];
-    bool local_light_active[HENKA_SCENE_MAX_LOCAL_LIGHTS];
-    henka_scene_fog_desc fog;
-};
+#include "scene/scene_internal.h"
 
 #endif
