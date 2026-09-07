f520412fb9c87e79b5425b503c8d9671f95be288
bconnell <brandon@brandonconnell.com>
2026-09-07 01:15:44 -0500

    Remove obsolete MCP startup diagnostics

--- examples/sandbox3d/main.c
+++ examples/sandbox3d/main.c
@@ -32314,11 +32314,6 @@ static henka_result sandbox3d_initialize(henka_engine* engine, void* user_data)
         HENKA_UI_THEME_DARK);
     if (result != HENKA_SUCCESS)
     {
-        if (state->mcp_stdio)
-        {
-            fprintf(stderr, "MCP initialization failed at main UI theme: %s.\n", henka_result_to_string(result));
-            fflush(stderr);
-        }
         return result;
     }
 
@@ -32333,11 +32328,6 @@ static henka_result sandbox3d_initialize(henka_engine* engine, void* user_data)
         HENKA_UI_THEME_DARK);
     if (result != HENKA_SUCCESS)
     {
-        if (state->mcp_stdio)
-        {
-            fprintf(stderr, "MCP initialization failed at native panel UI theme: %s.\n", henka_result_to_string(result));
-            fflush(stderr);
-        }
         return result;
     }
 
@@ -32356,11 +32346,6 @@ static henka_result sandbox3d_initialize(henka_engine* engine, void* user_data)
             HENKA_UI_THEME_DARK);
         if (result != HENKA_SUCCESS)
         {
-            if (state->mcp_stdio)
-            {
-                fprintf(stderr, "MCP initialization failed at detached panel UI theme %d: %s.\n", panel_index, henka_result_to_string(result));
-                fflush(stderr);
-            }
             return result;
         }
         henka_ui_set_visible(state->detached_panel_ui[panel_index], true);
@@ -34325,11 +34310,6 @@ static henka_result sandbox3d_initialize(henka_engine* engine, void* user_data)
     return HENKA_SUCCESS;
 
 fail:
-    if (state->mcp_stdio)
-    {
-        fprintf(stderr, "MCP initialization failed before stdio ready: %s.\n", henka_result_to_string(result));
-        fflush(stderr);
-    }
     henka_free(detail_normal_pixels);
     henka_free(macro_variation_pixels);
     henka_free(wood_grain_pixels);
@@ -37580,20 +37560,10 @@ int main(int argc, char** argv)
 #endif
 
     g_sandbox3d_mcp_stdio = mcp_stdio;
-    if (mcp_stdio)
-    {
-        fprintf(stderr, "MCP startup entered with the Release/Debug executable argument path.\n");
-        fflush(stderr);
-    }
     memset(&state, 0, sizeof(state));
     if (sandbox3d_authoring_asset_controller_create(
             &state.authoring_asset_controller) != HENKA_SUCCESS)
     {
-        if (mcp_stdio)
-        {
-            fprintf(stderr, "MCP startup failed while creating the authoring asset controller.\n");
-            fflush(stderr);
-        }
         HENKA_LOG_ERROR("Unable to allocate the native authoring controller");
         return 1;
     }
@@ -37710,11 +37680,6 @@ int main(int argc, char** argv)
     result = henka_engine_create(&config, &engine);
     if (result != HENKA_SUCCESS)
     {
-        if (mcp_stdio)
-        {
-            fprintf(stderr, "MCP startup failed while creating the engine: %s.\n", henka_result_to_string(result));
-            fflush(stderr);
-        }
         HENKA_LOG_ERROR("Unable to start the sandbox: %s", henka_result_to_string(result));
         return 1;
     }
@@ -37723,11 +37688,6 @@ int main(int argc, char** argv)
     result = henka_engine_run(engine);
     if (result != HENKA_SUCCESS)
     {
-        if (mcp_stdio)
-        {
-            fprintf(stderr, "MCP startup stopped with an engine error: %s.\n", henka_result_to_string(result));
-            fflush(stderr);
-        }
         HENKA_LOG_ERROR("The sandbox stopped with an error: %s", henka_result_to_string(result));
         henka_engine_destroy(engine);
         return 1;
